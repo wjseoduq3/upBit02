73ccbad40d6ace64085e4000f486ebdd9c8e6b50
wjseoduq2 <dragon65@naver.com>
2024-04-12 13:38:18 +0900

    텔레그램 비번 마스킹

--- telegramTest.py
+++ telegramTest.py
@@ -3,7 +3,7 @@
 import telegram
 import asyncio
 
-bot = telegram.Bot(token="7150770982:AAFSa3on25LTmBtIDDr9eqL8kleAtPiq7EI")
-chat_id = "7159357052"
+bot = telegram.Bot(token="**********")
+chat_id = "*******"
 
 asyncio.run(bot.sendMessage(chat_id=chat_id, text="Good Morning, My Python Telegram"))
